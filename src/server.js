cf28cfd62bcf82153bd9d7187536b6448ba58970
Lukas Gamer <lucasdevsoftware@gmail.com>
2020-07-22 22:20:16 -0300

    ok

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const ip = "192.168.1.66";
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (request, response) => response.sendFile("index.html"));
+app.listen(process.env.port || port, process.env.ip, () => console.log("Servidor rodando na porta "+port));
