fd77880b0bd9cfcf1531c75893d60c731b8b669d
Lukas Gamer <lucasdevsoftware@gmail.com>
2020-08-09 00:07:31 -0300

    Update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const ip = "192.168.1.66";
 
 app.use(express.static(__dirname + "/public"));
 
 app.get("/teste", (request, response) => response.sendFile("index.html"));
 app.listen(process.env.PORT || 3000, () => console.log("Servidor rodando!!!"));
+
