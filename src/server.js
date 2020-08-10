6c670dcfb13ff585104b75d31828cb9740c2bbe2
Lukas Gamer <lucasdevsoftware@gmail.com>
2020-08-10 17:54:04 -0300

    Update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
+app.get('/teste', (request, response) => response.sendFile('index.html'));
 
-app.get("/teste", (request, response) => response.sendFile("index.html"));
-app.listen(process.env.PORT || 3000, () => console.log("Servidor rodando!!!"));
+io.on('connection', (socket) => {
+    console.log(socket.id);
+});
 
+http.listen(process.env.PORT || 3000, () => console.log('Servidor rodando!!!'));
