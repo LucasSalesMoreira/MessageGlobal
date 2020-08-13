9da08bbbc82aba28e65f05d9720b3dc9c2403a1f
Lukas Gamer <lucasdevsoftware@gmail.com>
2020-08-13 02:11:11 -0300

    Update

--- src/server.js
+++ src/server.js
@@ -6,8 +6,15 @@ const io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 app.get('/teste', (request, response) => response.sendFile('index.html'));
 
+//---------- CRIA AS ROTAS MIKAIO.
+
 io.on('connection', (socket) => {
     console.log(socket.id);
+
+    socket.on('test1', (parameter) => {
+        console.log(parameter);
+    });
+
 });
 
 http.listen(process.env.PORT || 3000, () => console.log('Servidor rodando!!!'));
