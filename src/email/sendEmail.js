1215e9727396c1cc5b282fce9bd2f1d1302c194f
Lukas Gamer <lucasdevsoftware@gmail.com>
2020-08-19 08:42:29 -0300

    New function (new_user)

---
+++ src/email/sendEmail.js
@@ -0,0 +1,27 @@
+module.exports = (email) => {
+    const nodemailer = require('nodemailer');
+
+    const myEmail = 'lucasdevsoftware@gmail.com';
+    
+    const transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: myEmail,
+            pass: process.env.EMAIL_PASS,
+        }
+    });
+
+    var body = {
+        from: myEmail,
+        to: email,
+        subject: '<h1>Validação de email: 967921</h1>',
+        text: '<h3>Seja bem vindo ao nosso app!</h3>'
+    };
+
+    transport.sendMail(body, (error, info) => {
+        if (error)
+            console.log(`Falha no envio do email! -> ${error.message}`);
+        else
+            console.log(`Email enviado com sucesso! -> ${info.response}`);
+    });
+};
