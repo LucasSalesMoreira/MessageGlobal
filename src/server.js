aa0e23bbaeb880436542d092723d533b02ef2c8a
Lukas Gamer <lucasdevsoftware@gmail.com>
2020-08-21 20:37:02 -0300

    Test redirect

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 //---------- CRIA AS ROTAS MIKAIO.
 app.use(express.static(__dirname + '/public'));
 
-app.get('sendemail', (rec, res) => {
+app.get('/sendemail', (rec, res) => {
     res.send('https://lucassalestestes.000webhostapp.com/sendEmail.php?email=lucassalesmoreira161@gmail.com');
 });
 
