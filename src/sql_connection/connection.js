bbc4126e05683dbc963245db5b904a92f97f663c
Lukas Gamer <lucasdevsoftware@gmail.com>
2020-08-31 23:05:29 -0300

    Update

--- src/sql_connection/connection.js
+++ src/sql_connection/connection.js
@@ -68,13 +68,13 @@ module.exports = {
                 console.log(data.email+' --- '+this.results.email);
 
                 if (data.email === this.results.email) {
-                    this.create(`insert into User (name, email, password) values ('${this.results.name}', '${this.results.email}', '${data.password}'`);
+                    this.create(`insert into User (name, email, password) values ('${this.results.name}', '${this.results.email}', '${data.password}')`);
                     this.delete(`delete from authentication where code = '${this.results.code}'`);
                     socket.emit('_authenticating', {ok: true});
                 } else {
                     socket.emit('_authenticating', {ok: false});
                 }
-                
+
                 this.results = null;
             } else {
                 socket.emit('_authenticating', {ok: false});
