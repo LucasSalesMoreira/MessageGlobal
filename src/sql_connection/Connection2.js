788f69cef7ad91c448dbd8853c5f2b6c9e1cbfce
RPA 1 <RPA 1@EMLDVDPN01.demprel.local>
2020-09-05 15:40:08 -0300

    Update

---
+++ src/sql_connection/Connection2.js
@@ -0,0 +1,29 @@
+module.exports = class Connection {
+    constructor() {
+        this.host_db = process.env.host_db;
+        this.port_db = process.env.port_db;
+        this.user_db = process.env.user_db;
+        this.pass_db = process.env.pass_db;
+        this.db = process.env.db;
+    }
+
+    create() {
+
+    }
+
+    search() {
+
+    }
+
+    authentication() {
+
+    }
+
+    update() {
+
+    }
+
+    delete() {
+
+    }
+}
