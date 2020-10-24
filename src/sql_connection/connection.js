8c943fe6f084d8735c2fa5f3d580617b2bca4b22
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2020-10-24 01:05:54 -0300

    comment removed

--- src/sql_connection/connection.js
+++ src/sql_connection/connection.js
@@ -148,7 +148,6 @@ module.exports = {
     },
 
     loadContacts: function(email, socket) {
-        // new sql -> select c.email_contact, u.name from contacts c join User u on u.email = c.email_contact where c.email_user = "lucas@gmail.com";
         this.searsh(`select c.email_contact, u.name from contacts c join User u on u.email = c.email_contact where c.email_user = '${email}'`);
         setTimeout(() => {
             console.log(this.resultsArray);
